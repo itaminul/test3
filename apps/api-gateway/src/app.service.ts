f0e8aaed4895c578bdf873cd8223af8fa4d231b3
itaminul <itaminul@gmail.com>
2024-10-28 22:07:49 +0600

    order created

--- apps/api-gateway/src/app.service.ts
+++ apps/api-gateway/src/app.service.ts
@@ -1,17 +1,21 @@
-import { Inject, Injectable } from '@nestjs/common';
-import { CreateOrderDto } from './app.dto';
-import { ClientKafka } from '@nestjs/microservices';
+import { Inject, Injectable } from "@nestjs/common";
+import { CreateOrderDto } from "./app.dto";
+import { ClientKafka } from "@nestjs/microservices";
+import { OrderCreateEvent } from "./order-created";
 
 @Injectable()
 export class AppService {
   constructor(
-    @Inject('BILLING_SERVICE') private readonly billingProxyClient: ClientKafka,
+    @Inject("BILLING_SERVICE") private readonly billingProxyClient: ClientKafka
   ) {}
   getHello(): string {
-    return 'Hello World!';
+    return "Hello World!";
   }
 
   createOrder(payload: CreateOrderDto) {
-    this.billingProxyClient.emit();
+    this.billingProxyClient.emit(
+      "order_created",
+      new OrderCreateEvent(payload.id, payload.name, payload.price)
+    );
   }
 }
